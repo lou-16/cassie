691cdac071991b63bfccb0e900d18acb6a80cadc
lou-16 <gurnoor16singh@gmail.com>
2025-06-16 20:47:27 +0530

    shiet i misssed somethig

--- src/buildserviceimpl.h
+++ src/buildserviceimpl.h
@@ -6,4 +6,5 @@ class BuildServiceImpl : public BuildService {
         int addJobToQueue(const std::string location, const BUILD_TYPE typeofBuild);
         int executeJob(const std::string id);
 
+        int setJobStatus(const std::string id, const JOB_STATUS status) override;
 };
