7ac328d967fe1f140680526835707f826c2dd4f9
lou <lou@192.168.1.200>
2025-10-12 01:06:17 +0530

    [ContainerService] removed GitService deps + provision for streamlined API integration.

--- src/Docker/containerservice.h
+++ src/Docker/containerservice.h
@@ -21,62 +21,50 @@ class ContainerService {
     private:
         httplib::Client client;
     public:
+        /* TODO : Switch this out for a non-blocking socket I/O lib */
         ContainerService() : client("unix://var/run/docker.sock") {
             client.set_connection_timeout(5); //seconds
             client.set_read_timeout(5);
             client.set_write_timeout(5);
         };
-        static std::vector<std::shared_ptr<ContainerInfo>> containers; 
+        /* map that "maps" (lol) the jobID to the ContainerInfo struct.  */
         static std::unordered_map<std::string,std::shared_ptr<ContainerInfo>> containersMap;
-        static std::mutex containersMutex;
-        static std::condition_variable isAvailable;
+        static std::mutex containersMutex; /* mutex for the containersMap */
+        /* we lock the STL container due to multithreaded access being UB */
 
-        static std::vector< std::thread > stdioThreads;
-        static const int max_size_threads = 10;
-        static void stdio_function();
-
-        std::shared_ptr<ContainerInfo> findContainer(const std::string& id); //
-        bool startContainer(const std::string& name); //
-        bool stopContainer(const std::string& name); //
-        std::string getContainerStatus(const std::string& name); //
+        /* class methods / API for communication with the Docker UNIX socket */
+        std::shared_ptr<ContainerInfo> findContainer(const std::string& JobID); //
+        bool startContainer(const std::string& JobID); //
+        bool stopContainer(const std::string& JobID); //
+        std::string getContainerStatus(const std::string& JobID); //
         ~ContainerService() = default; // 
-        std::shared_ptr<ContainerInfo> intialiseContainerInfo(const Job& J); //
-        std::string createContainer(const Job& J); //
-        void initialiseThreads();
+        std::shared_ptr<ContainerInfo> initialiseContainerInfo(const std::shared_ptr<Job> J, const json& config); //
+        std::string createContainer(const std::shared_ptr<Job> J); //
+
+        /* TODO (IMPORTANT) : extend this class to allow for TCP streams to be sent to the backend */
 };
 
-std::shared_ptr<ContainerInfo> ContainerService::intialiseContainerInfo(const Job& job_ref) {
+std::shared_ptr<ContainerInfo> ContainerService::initialiseContainerInfo(std::shared_ptr<Job> job_ref, const json& config) {
     {
+        // lock the mutex for exclusive access
         std::unique_lock lck(containersMutex);
     }
     try {
-        std::string filePath = job_ref.location + "/cassie-config.json";
-        std::ifstream file(filePath);
-        if(!file.is_open()){
-            throw std::runtime_error("[-] intialiseContainerInfo failed");
-        }
-        json j;
-        try {
-            file >> j;
-        }   catch (json::parse_error& e){
-            std::string error = "[-] failed to parse cassie-config.json in intialiseContainerInfo for ContainerService";
-            throw std::runtime_error(error);
-        }
+         
         auto newContainer = std::make_shared<ContainerInfo>();
-        newContainer.get()->jobId = job_ref.__id;
+        newContainer.get()->jobId = job_ref->__id;
         {
-            j.at("baseImage").get_to(newContainer->Image);
-            if (j.contains("workingDirectory")) j.at("workingDirectory").get_to(newContainer->workingDir);
-            if (j.contains("copyPaths")) j.at("copyPaths").get_to(newContainer->copyPaths);
-            if (j.contains("aptPackages")) j.at("aptPackages").get_to(newContainer->aptPkgs);
-            if (j.contains("buildCommands")) j.at("buildCommands").get_to(newContainer->buildCommands);
-            if (j.contains("entrypoint")) j.at("entrypoint").get_to(newContainer->entryPoint);
-            if (j.contains("cmd")) j.at("cmd").get_to(newContainer->cmd);
-            if (j.contains("exposedPorts")) j.at("exposedPorts").get_to(newContainer->exposedPorts);
-            if (j.contains("envVars")) j.at("envVars").get_to(newContainer->envVars);
-            if (j.contains("volumes")) j.at("volumes").get_to(newContainer->volumes);
+            config.at("baseImage").get_to(newContainer->Image);
+            if (config.contains("workingDirectory")) config.at("workingDirectory").get_to(newContainer->workingDir);
+            if (config.contains("copyPaths")) config.at("copyPaths").get_to(newContainer->copyPaths);
+            if (config.contains("aptPackages")) config.at("aptPackages").get_to(newContainer->aptPkgs);
+            if (config.contains("buildCommands")) config.at("buildCommands").get_to(newContainer->buildCommands);
+            if (config.contains("entrypoint")) config.at("entrypoint").get_to(newContainer->entryPoint);
+            if (config.contains("cmd")) config.at("cmd").get_to(newContainer->cmd);
+            if (config.contains("exposedPorts")) config.at("exposedPorts").get_to(newContainer->exposedPorts);
+            if (config.contains("envVars")) config.at("envVars").get_to(newContainer->envVars);
+            if (config.contains("volumes")) config.at("volumes").get_to(newContainer->volumes);
         }
-        containers.push_back(newContainer);
         containersMap[newContainer.get()->id] = newContainer;
         return newContainer;
     } catch (std::runtime_error& e){
@@ -111,53 +99,57 @@ bool ContainerService::startContainer(const std::string& name) {
     return res.status == 204;
 }
 
-std::string ContainerService::createContainer(const Job& j) {
+/* before calling, verify whether the job ptr passed has a valid docker body that can directly be put into the POST req*/
+std::string ContainerService::createContainer(const std::shared_ptr<Job> j) {
 
-    
-
-    std::shared_ptr<ContainerInfo> Container  = intialiseContainerInfo(j);
-    
+    /* refs and endpoints along with POST req body and headers*/
+    std::shared_ptr<ContainerInfo> Container  = containersMap[j.get()->__id];    
     std::string endpoint = "/1.41/containers/create";
-
-    json body = {
-        //implement proper image support, for now, just get hello-world working
-        {"Image" , "hello-world"}
-    };
-
-
+    json body = j.get()->config;
+    /* TODO : remove httplib dependency */
     httplib::Headers headers = {
         {"Content-Type" , "application/json"}
     };
-
+    /* Fetch Response of POST request */
     FetchResponse res = fetch(client, endpoint, "POST", headers, body.dump());
 
-    {
-        std::unique_lock lock(containersMutex);
-        isAvailable.notify_one();
-    }
-
+    /* json that will stored info to be parsed. */
     json responseToBeParsed = json::parse(res.body, nullptr, false);
+
+    /* if response is fine, then set container's id to docker container id*/
     if(res.status == 201 && responseToBeParsed.contains("id")){
         Container.get()->id = responseToBeParsed["id"];
-    } else if (responseToBeParsed.contains("message")){
+    } 
+    /* if response from the socket contains a message */
+    else if (responseToBeParsed.contains("message")){
+        /* set id to empty str */
         Container.get()->id = "";
-
-        return "";
+        /* return an error that can be straight up sent to the backend */
+        return "ERROR_" + responseToBeParsed["message"];
     }
     else{
-        return "";
+        /* if anything wack happens */
+        return "FAILURE";
     }
+    /* return the container id, which can be stored in db/cache, linking to the jobID*/
     return Container.get()->id;
     
 }
 
-bool ContainerService::stopContainer(const std::string& id) {
-    std::shared_lock lock(containersMutex);
-    std::shared_ptr<ContainerInfo> container = findContainer(id);
+bool ContainerService::stopContainer(const std::string& containerId) {
+    /* find the pointer to the container */
+    std::shared_ptr<ContainerInfo> container = findContainer(containerId);
+    
+    /* if null pointer */
     if (!container) {
         return false;
     }
+    /* if createContainer failed, this container should not exist, but just a sanity check*/
     if(container.get()->id == ""){
+        /* if such a container is found, we simply remove it from map, stop it from polluting it */
+        containersMap.erase(container.get()->jobId);
+        /* call the destructor for this pointer */
+        container.~shared_ptr();
         return false;
     }
     std::string endpoint = "/1.41/containers/" + container.get()->id + "/kill";
@@ -173,7 +165,10 @@ std::string ContainerService::getContainerStatus(const std::string& id){
     if(!container) {
         return "CONTAINER_NOT_FOUND";
     }
+    /* damn sometimes i overcheck dude. this SHOULD NOT EXIST. hmph */
     if(container.get()->id == ""){
+        containersMap.erase(container.get()->jobId);
+        container.~shared_ptr();
         return "CONTAINER_NOT_INIT";
     }
     std::string endpoint = "/1.41/containers/" + container.get()->id + "/json";
@@ -192,16 +187,4 @@ std::string ContainerService::getContainerStatus(const std::string& id){
 
 }
 
-void ContainerService::initialiseThreads() {
-    for(int i = 0; i < max_size_threads; i++) {
-        stdioThreads[i] = std::thread(stdio_function);
-    }
-    return;
-}
 
-void ContainerService::stdio_function() {
-    while(true) {
-        std::unique_lock lck(containersMutex);
-        isAvailable.wait(lck, [](){return !ContainerService.})
-    }
-}
